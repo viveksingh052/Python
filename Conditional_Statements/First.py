251d95de63ac48a182b1a7c5e3bcf46eb093d87e
vivek singh <vs292713@gmail.com>
2025-05-07 18:19:14 +0530

    First File

---
+++ Conditional_Statements/First.py
@@ -0,0 +1,11 @@
+# Area of the circle
+
+pi = 3.147
+radius = float(input("Enter the radius value : "))
+
+if(type(radius) == float or int): 
+    Area_of_circle = pi * radius**2
+    print(f" Area of circle is : {Area_of_circle}")
+
+
+
