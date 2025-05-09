7b1c447849954c876fd8574adfd99436f67efd0a
vivek singh <vs292713@gmail.com>
2025-05-09 22:25:38 +0530

    Question

---
+++ Loops/questions.py
@@ -0,0 +1,20 @@
+#write a program to return the even numbers from 1 to 10
+
+# i = 1
+
+# while i <=10:
+#     if(i%2==0):
+#         print(i)
+    
+#     i+=1
+
+# for i in range(1,11):
+#     if(i%2==0):
+#         print(i)
+
+#program to find the odd numbers in the given range from 1 to 10 
+
+for i in range(1,11):
+    if(i%2!=0):
+        print(i)
+        
