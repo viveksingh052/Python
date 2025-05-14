3014f6e30010898ecd085ac3105ecf7170b82d2c
vivek singh <vs292713@gmail.com>
2025-05-14 14:28:52 +0530

    Question

---
+++ Questions_till_tuples/Medium/Question8.py
@@ -0,0 +1,7 @@
+#8. Create a tuple with five elements. Try to change the second element. What happens?
+
+fruits = ("mango","papaya","pineapple","litchi","guava")
+
+fruits[1] = "papaya"
+
+#does not change beacuse typles are immutable
