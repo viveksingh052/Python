a2086a9eff3398b62b838adae9602246024b2c18
vivek singh <vs292713@gmail.com>
2025-05-14 14:25:31 +0530

    Question

---
+++ Questions_till_tuples/Hard/Question6.py
@@ -0,0 +1,6 @@
+'''6. Create a list of tuples, where each tuple contains a student name and their marks.
+Write a program to find the student with the highest marks.'''
+
+
+student = (("vivek",95),("shobhit",88),("pritika",33))
+
