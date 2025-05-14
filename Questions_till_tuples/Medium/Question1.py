00ed26dfcb4e8e90063460dd84bd3e16bbaa9406
vivek singh <vs292713@gmail.com>
2025-05-14 14:27:33 +0530

    Question

---
+++ Questions_till_tuples/Medium/Question1.py
@@ -0,0 +1,10 @@
+#1.1. Write a Python program to swap two variables without using a third variable.
+
+a = 10 
+b = 15 
+
+#we can do with this approach
+
+a,b = b,a 
+
+print(f"The value of a : {a} , The value of b : {b}")
