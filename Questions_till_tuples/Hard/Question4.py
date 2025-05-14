a8837b8e2aaca6a6dab7fbceb6d99b3101af49db
vivek singh <vs292713@gmail.com>
2025-05-14 14:25:01 +0530

    Question

---
+++ Questions_till_tuples/Hard/Question4.py
@@ -0,0 +1,5 @@
+# Explain how memory is managed in Python
+
+Python uses a private heap space for memory management.
+This is where all Python objects and data structures are stored.
+You don’t need to manually allocate or free memory — Python handles it for you.
