8e5eeedb731fd9e758cedbeded21b75f57c7c11c
vivek singh <vs292713@gmail.com>
2025-05-16 00:17:06 +0530

    Add

---
+++ Dictionary/Operations/add.py
@@ -0,0 +1,9 @@
+my_dict = {
+    "Name" : "vivek Singh" , "Profile":"Data Analyst"
+}
+
+print(my_dict)
+
+my_dict["Domain"] = "Data Science" #to add a key value pair in dictionary
+print(my_dict)
+
